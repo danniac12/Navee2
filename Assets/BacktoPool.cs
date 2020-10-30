a9c8b424300dcf66cc314ff5945631dc810c9359
Daniel Aguirre <da.ac47@gmail.com>
2020-10-29 23:29:20 -0500

    Initial commit

---
+++ Assets/BacktoPool.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BacktoPool : MonoBehaviour
+{
+    public Transform safeBox;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Rock"))
+        {
+            collision.transform.position = new Vector2(safeBox.position.x, safeBox.position.y);
+            collision.gameObject.SetActive(false);
+        }
+        if (collision.CompareTag("Clock"))
+        {
+            collision.transform.position = new Vector2(safeBox.position.x, safeBox.position.y);
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
